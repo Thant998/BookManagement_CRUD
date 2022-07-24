799b98d5ea22a6f761964b24bddf622446e74a14
cry_babe <95152361+Thant998@users.noreply.github.com>
2022-07-24 19:15:26 +0630

    Add files via upload

---
+++ bookmanagement.controller/DeleteServlet.java
@@ -0,0 +1,50 @@
+package bookmanagement.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bookmanagement.dao.BookDAO;
+import bookmanagement.dto.BookRequestDTO;
+
+/**
+ * Servlet implementation class DeleteServlet
+ */
+@WebServlet("/DeleteServlet")
+public class DeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String code=request.getParameter("code");
+		BookDAO dao=new BookDAO();
+		BookRequestDTO dto=new BookRequestDTO();
+		dto.setBookCode(code);
+		dao.deleteData(dto);
+		
+		response.sendRedirect("DisplayBookServlet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
